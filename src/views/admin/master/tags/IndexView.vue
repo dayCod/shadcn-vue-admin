a996c5dbfe3943e43600ff410809e46bd10bcb7d
dayCod <alayarahman07@gmail.com>
2025-12-23 10:57:44 +0700

    feat: adding action button

--- src/views/admin/master/tags/IndexView.vue
+++ src/views/admin/master/tags/IndexView.vue
@@ -1,16 +1,18 @@
 <script setup>
 import { h } from 'vue'
 import Datatable from '@/components/Datatable.vue'
-import { ArrowUpDown, PlusIcon } from 'lucide-vue-next'
+import { ArrowUpDown, Edit, PlusIcon, Trash2 } from 'lucide-vue-next'
 import { Button } from '@/components/ui/button'
 import Card from '@/components/ui/card/Card.vue'
 import CardHeader from '@/components/ui/card/CardHeader.vue'
 import CardContent from '@/components/ui/card/CardContent.vue'
 import CardTitle from '@/components/ui/card/CardTitle.vue'
+
 const data = [
   { id: '1', amount: 100, status: 'pending', email: 'm@example.com' },
   { id: '2', amount: 200, status: 'success', email: 'test@example.com' },
 ]
+
 const columns = [
   {
     accessorKey: 'email',
@@ -32,15 +34,26 @@ const columns = [
     cell: ({ row }) => h('div', { class: 'capitalize' }, row.getValue('status')),
   },
   {
-    accessorKey: 'amount',
-    header: () => h('div', { class: 'text-right' }, 'Amount'),
+    accessorKey: 'action',
+    header: () => h('div', { class: 'text-right' }, 'Action'),
     cell: ({ row }) => {
-      const amount = Number.parseFloat(row.getValue('amount'))
-      const formatted = new Intl.NumberFormat('en-US', {
-        style: 'currency',
-        currency: 'USD',
-      }).format(amount)
-      return h('div', { class: 'text-right font-medium' }, formatted)
+      return h('div', { class: 'flex gap-2 justify-end' }, [
+        h(
+          Button,
+          {
+            onClick: () => console.log(row.original.id),
+          },
+          () => [h(Edit)],
+        ),
+        h(
+          Button,
+          {
+            variant: 'destructive',
+            onClick: () => console.log(row.original.id),
+          },
+          () => [h(Trash2)],
+        ),
+      ])
     },
   },
 ]
